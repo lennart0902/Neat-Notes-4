1fc94b4873e4be5247c533f8c3f04da6a58f4811
lennart0902 <147739629+lennart0902@users.noreply.github.com>
2024-05-02 16:45:32 +0200

    datenbank angelegt

---
+++ database/migrations/1714660958082_create_notizens_table.ts
@@ -0,0 +1,20 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'notizens'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').primary()
+      table.string('name').notNullable()
+      table.text('inhalt')
+
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
