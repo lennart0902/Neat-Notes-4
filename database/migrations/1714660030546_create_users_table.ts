f957208f838f5310a4f1e5ffeb25c963d92ac1fb
lennart0902 <147739629+lennart0902@users.noreply.github.com>
2024-05-16 15:27:52 +0200

    weitere seiten angelegt

--- database/migrations/1714660030546_create_users_table.ts
+++ database/migrations/1714660030546_create_users_table.ts
@@ -18,3 +18,4 @@ export default class extends BaseSchema {
     this.schema.dropTable(this.tableName)
   }
 }
+
--- start/routes.ts
+++ start/routes.ts
@@ -7,9 +7,16 @@
 |
 */
 
+import { Router } from '@adonisjs/core/http'
 import router from '@adonisjs/core/services/router'
 
 router.on('/').render('pages/home')
 
 
-Route.get('/', async ({ view }) => {return view.render('home')})
+import Route from '@ioc:Adonis/Core/Route'
+
+Route.on('/').render('pages/home')
+
+Route.get('kategories', async ({ view }) => {
+    return view.render('kategories')
+})
