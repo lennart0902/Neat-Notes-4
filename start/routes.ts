270651b842a8cac9230f12eba9d3c433b650862e
LennyBuck <lb224@hdm-stuttgart.de>
2024-06-06 14:47:24 +0200

    nicht beachten
    ausversehn upsi pupsi

--- start/routes.ts
+++ start/routes.ts
@@ -7,16 +7,12 @@
 |
 */
 
-import { Router } from '@adonisjs/core/http'
 import router from '@adonisjs/core/services/router'
 
 router.on('/').render('pages/home')
 
 
-import Route from '@ioc:Adonis/Core/Route'
 
-Route.on('/').render('pages/home')
-
-Route.get('kategories', async ({ view }) => {
+router.get('/kategories', async ({ view }) => {
     return view.render('kategories')
 })
