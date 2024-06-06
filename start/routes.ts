fde708cbb280218b003e0b05980631c238c2082b
LennyBuck <lb224@hdm-stuttgart.de>
2024-06-06 15:03:44 +0200

    conflict zusammen geführt

--- start/routes.ts
+++ start/routes.ts
@@ -6,20 +6,22 @@
 | The routes file is used for defining the HTTP routes.
 |
 */
-<<<<<<< HEAD
 
 import router from '@adonisjs/core/services/router'
 
-router.on('/').render('pages/home')
+router.on('/z').render('pages/home')
 
 
-=======
 //import router from '@adonisjs/core/services/router'
 import Route from '@ioc:Adonis/Core/Route'
->>>>>>> fff6b7d24a41a05231a0bf880157ea5f2340514d
 
 router.get('/kategories', async ({ view }) => {
     return view.render('kategories')
+})
+
+router.get('/', async ({ view }) => {
+    return view.render('pages/index')
+
 })
 //Router.get('/kategories', async ({ view }) => {
 //    return view.render('pages/kategories')
